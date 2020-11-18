4e135c3db95a098649c309f21b40aaa634ae2815
Andre Hermanto <andre.hermanto93@gmail.com>
2020-11-19 05:06:54 +1100

    Update DB

--- mysql/VECTIS_ANALYTICS.sql
+++ mysql/VECTIS_ANALYTICS.sql
@@ -2,11 +2,11 @@ CREATE DATABASE VECTIS_ANALYTICS;
 
 USE VECTIS_ANALYTICS;
 
-CREATE TABLE USER_QUERIES (
-    timestamp DATETIME,
-    email VARCHAR(255),
-    platform  VARCHAR(255),
-    cohort VARCHAR(255)
+CREATE TABLE USER_QUERIES ( 
+	timestamp DATETIME, 
+    email VARCHAR(255), 
+    cohort VARCHAR(255),
+	data_access VARCHAR(255)
 );
 
 CREATE TABLE SEARCH_QUERIES (
@@ -14,15 +14,16 @@ CREATE TABLE SEARCH_QUERIES (
 	gene varchar(255),
 	panel_source varchar(255),
 	panel varchar(255),
-	platform varchar(255) NOT NULL,
 	cohort varchar(255) NOT NULL,
 	data_access varchar(255) NOT NULL
 );
 
 CREATE TABLE USER_LOGIN (
-    timestamp datetime NOT NULL,
-    email VARCHAR(255),
-    platform varchar(255) NOT NULL
+	timestamp datetime NOT NULL,
+    email VARCHAR(255), 
+	platform varchar(255) NOT NULL,
+	signup boolean,
+	geo GEOMETRY
 );
 
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
